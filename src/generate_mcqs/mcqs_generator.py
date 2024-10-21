478f76b0ccef8cc9cde341118e4290782f4f875e
Dilesh Bisen <1dileshbisen@gmail.com>
2024-10-21 22:53:51 +0530

    update file

--- src/generate_mcqs/mcqs_generator.py
+++ src/generate_mcqs/mcqs_generator.py
@@ -19,6 +19,8 @@ load_dotenv()
 
 my_key = os.getenv("MISTRAL_API_KEY")
 
+if my_key is None:
+    raise ValueError("MISTRAL_API_KEY is not set. Please check your .env file.")
 
 llm = ChatMistralAI(
     api_key=my_key,
